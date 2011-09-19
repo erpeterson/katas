ee19970df42986139bc41d3be9fccb1c34e01203
Aeden Jameson <aeden.jameson@gmail.com>
2011-09-19 01:05:44 -0700

    Attempt 1 (C#): converting I to 1 fails
      Attempt 1 (C#): Red
    
    Attempt (C#): It actually makes more sense to organize by person
    
    Attempt (C#): Red

---
+++ Me/RomanNumerals/CSharp/given_a_roman_numeral.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+
+namespace CSharp
+{
+
+    [TestFixture]
+    public class given_a_roman_numeral
+    {
+        [SetUp]
+        protected void Context()
+        {
+
+        }
+
+
+        [TestCase("I", 1, TestName = "then I is converted 1")]
+        public void when_converting_it_to_an_arabic_numeral(string Roman, int ExpectedArabic)
+        {
+            Roman.ToArabic().Should().Be(ExpectedArabic);
+        }
+
+    }
+
+
+    public static class RomanNumeralConverter
+    {
+        public static int ToArabic(this string Roman)
+        {
+            return 0;
+        }
+    }
+}
