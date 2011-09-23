b958f8a10575ecfa772c8888d207f50f10c53f8b
Aeden Jameson <aeden.jameson@gmail.com>
2011-09-23 13:54:12 -0700

    Hall&Me - Attempt 1 (C#): converting VI to 6 fails

--- AdronHallAndMe/RomanNumerals/CSharp/given_a_roman_numeral.cs
+++ AdronHallAndMe/RomanNumerals/CSharp/given_a_roman_numeral.cs
@@ -2,8 +2,6 @@
 using FluentAssertions;
 using NUnit.Framework;
 
-
-
 namespace CSharp
 {
     [TestFixture]
@@ -14,6 +12,7 @@ namespace CSharp
         [TestCase("III", 3, TestName = "then III is converted to 3")]
         [TestCase("IV", 4, TestName = "then IV is converted to 4")]
         [TestCase("V", 5, TestName = "then V is converted to 5")]
+        [TestCase("VI", 6, TestName = "then VI is converted to 6")]
         public void when_converting_to_an_arabic(string Roman, int ExpectedArabic)
         {
             Roman.ToArabic().Should().Be(ExpectedArabic);
@@ -25,7 +24,7 @@ namespace CSharp
         public static int ToArabic(this string Roman)
         {
             if (Roman == "V") return 5;
-            return Roman == "IV" ? 4 :Roman.Count();
+            return Roman == "IV" ? 4 : Roman.Count();
         }
     }
 }
