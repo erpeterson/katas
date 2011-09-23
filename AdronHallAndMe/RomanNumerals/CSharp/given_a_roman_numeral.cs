7ee2068f1bdc9d1ebe2b99573e47102b55b5b73a
Aeden Jameson <aeden.jameson@gmail.com>
2011-09-23 13:39:27 -0700

    Hall&Me - Attempt 1 (C#): converting I to 1 fails

---
+++ AdronHallAndMe/RomanNumerals/CSharp/given_a_roman_numeral.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+
+
+namespace CSharp
+{
+    [TestFixture]
+    public class given_a_roman_numeral
+    {
+        [TestCase("I", 1, TestName = "then I is converted to 1")]
+        public void when_converting_to_an_arabic(string Roman, int ExpectedArabic)
+        {
+            Roman.ToArabic().Should().Be(ExpectedArabic);
+        }
+
+    }
+
+
+    public static class RomanToArabicConverter
+    {
+        public static int ToArabic(this string Romain)
+        {
+            return 0;
+        }
+    }
+}
