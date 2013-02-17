01af255c79f03232c7687161a48860a70892654e
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-17 11:48:01 -0800

    Attempt 1 (C#): converting MCMXCVIII to 1998 passes

--- Me/RomanNumerals/CSharp/given_a_roman_numeral.cs
+++ Me/RomanNumerals/CSharp/given_a_roman_numeral.cs
@@ -32,6 +32,7 @@ namespace CSharp {
     [TestCase("D", 500, TestName = "then D is converted 500")]
     [TestCase("CM", 900, TestName = "then CM is converted 900")]
     [TestCase("M", 1000, TestName = "then M is converted 1000")]
+    [TestCase("MCMXCVIII", 1998, TestName = "then MCMXCVIII is converted 1998")]
     public void when_converting_it_to_an_arabic_numeral(string Roman, int ExpectedArabic) {
       Roman.ToArabic().Should().Be(ExpectedArabic);
     }
