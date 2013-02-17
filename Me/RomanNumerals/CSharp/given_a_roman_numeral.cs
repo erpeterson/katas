a557375313928916c50dc70779cf0129297686b3
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-17 11:40:09 -0800

    Attempt 1 (C#): converting CD to 400 passes

--- Me/RomanNumerals/CSharp/given_a_roman_numeral.cs
+++ Me/RomanNumerals/CSharp/given_a_roman_numeral.cs
@@ -38,7 +38,7 @@ namespace CSharp {
   public static class RomanNumeralConverter {
     public static int ToArabic(this string Roman) {
       IDictionary<string, int> SimpleNumerals = new Dictionary<string, int> {{"I", 1}, {"V", 5}, {"X", 10}, {"L", 50}, {"C", 100}, {"D", 500}};
-      IDictionary<string, int> CompoundNumerals = new Dictionary<string, int> {{"IV", 4}, {"IX", 9}, {"XL", 40}, {"XC", 90}};
+      IDictionary<string, int> CompoundNumerals = new Dictionary<string, int> {{"IV", 4}, {"IX", 9}, {"XL", 40}, {"XC", 90}, {"CD", 400}};
 
       var Total = 0;
       while (Roman.Length > 0) {
