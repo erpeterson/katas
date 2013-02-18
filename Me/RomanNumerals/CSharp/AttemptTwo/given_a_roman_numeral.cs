7a1b1af69ff3288693c7e6f6b9fc1ca86cb63446
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 15:23:55 -0800

    Attempt 2 (C#): converting IX to 9 fails

--- Me/RomanNumerals/CSharp/AttemptTwo/given_a_roman_numeral.cs
+++ Me/RomanNumerals/CSharp/AttemptTwo/given_a_roman_numeral.cs
@@ -6,6 +6,7 @@ namespace CSharp.AttemptTwo {
   public class given_a_roman_numeral {
     [TestCase("I", 1, TestName = "then I is converted to 1")]
     [TestCase("IV", 4, TestName = "then IV is converted to 4")]
+    [TestCase("IX", 9, TestName = "then IX is converted to 9")]
     public void when_converting_it_to_an_arabic_numeral(string Roman, int ExpectedArabic) {
       Roman.ToArabic().Should().Be(ExpectedArabic);
     }
