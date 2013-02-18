fca085f7014744d271d54e17ff972ab16f7db756
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 15:25:47 -0800

    Attempt 2 (C#): converting II to 2 fails

--- Me/RomanNumerals/CSharp/AttemptTwo/given_a_roman_numeral.cs
+++ Me/RomanNumerals/CSharp/AttemptTwo/given_a_roman_numeral.cs
@@ -5,6 +5,7 @@ namespace CSharp.AttemptTwo {
   [TestFixture]
   public class given_a_roman_numeral {
     [TestCase("I", 1, TestName = "then I is converted to 1")]
+    [TestCase("II", 2, TestName = "then II is converted to 2")]
     [TestCase("IV", 4, TestName = "then IV is converted to 4")]
     [TestCase("IX", 9, TestName = "then IX is converted to 9")]
     public void when_converting_it_to_an_arabic_numeral(string Roman, int ExpectedArabic) {
