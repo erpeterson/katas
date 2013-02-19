dd46122766c770fe4233cd3177ece405adfb6c45
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 19:06:35 -0800

    Attempt 1 (Ruby): converting IV to 4 fails

--- Me/RomanNumerals/ruby/roman_numeral_spec.rb
+++ Me/RomanNumerals/ruby/roman_numeral_spec.rb
@@ -4,7 +4,8 @@ describe "given a roman numeral" do
   context "when converting it to an arabic numeral" do
     {
       "" => 0,
-      "I" => 1
+      "I" => 1,
+      "IV" => 4
     }.each_pair do |roman, arabic|
       it "converts #{roman} string to #{arabic}" do
         expect(convert(roman)).to eq(arabic)
