feec16ebef1efa2271637ce049893d1748a7c240
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 16:27:29 -0800

    Attempt 2 (C#): converting V to 5 fails

--- Me/RomanNumerals/CSharp/AttemptTwo/given_a_roman_numeral.cs
+++ Me/RomanNumerals/CSharp/AttemptTwo/given_a_roman_numeral.cs
@@ -10,6 +10,7 @@ namespace CSharp.AttemptTwo {
     [TestCase("II", 2, TestName = "then II is converted to 2")]
     [TestCase("III", 3, TestName = "then III is converted to 3")]
     [TestCase("IV", 4, TestName = "then IV is converted to 4")]
+    [TestCase("V", 5, TestName = "then V is converted to 5")]
     [TestCase("IX", 9, TestName = "then IX is converted to 9")]
     public void when_converting_it_to_an_arabic_numeral(string Roman, int ExpectedArabic) {
       Roman.ToArabic().Should().Be(ExpectedArabic);
