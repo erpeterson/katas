13613444590e7d211f98d15638613b508893f9b1
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 21:53:52 -0800

    Attempt 1 (Ruby): converting VI to 6 fails

--- Me/RomanNumerals/ruby/roman_numeral_spec.rb
+++ Me/RomanNumerals/ruby/roman_numeral_spec.rb
@@ -6,7 +6,8 @@ describe "given a roman numeral" do
       "II" => 2,
       "III" => 3,
       "IV" => 4,
-      "V" => 5
+      "V" => 5,
+      "VI" => 6
     }.each_pair do |roman, arabic|
       it "converts #{roman} string to #{arabic}" do
         expect(convert(roman)).to eq(arabic)
