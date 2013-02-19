4a1cc016335119ce8176d7be197b0df1e7f331a9
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 17:17:18 -0800

    Attempt 3 (C#): refactor multiple if statements to dictionary lookup

--- Me/RomanNumerals/CSharp/Attempt 3/given_a_roman_numeral.cs
+++ Me/RomanNumerals/CSharp/Attempt 3/given_a_roman_numeral.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace CSharp.AttemptThree {
@@ -9,19 +11,21 @@ namespace CSharp.AttemptThree {
     [TestCase("II", 2, TestName = "then II is converted to 2")]
     [TestCase("III", 3, TestName = "then III is converted to 3")]
     [TestCase("IV", 4, TestName = "then IV is converted to 4")]
-    [TestCase("V", 5, TestName = "then V is converted to 5")]
+    [TestCase("V", 5, TestName = "then V is converted to 5")]    
     public void when_converting_it_to_an_arabic_numeral(string Roman, int ExpectedArabic) {
       Roman.ToArabic().Should().Be(ExpectedArabic);
     }
   }
 
   public static class RomanNumeralConverter {   
-    public static int ToArabic(this string Roman) {
+    static readonly IDictionary<string, int> Conversions = new Dictionary<string, int>{{"V", 5}, {"IV", 4}, {"I", 1}};
+
+    public static int ToArabic(this string Roman) {      
       if (string.IsNullOrEmpty(Roman)) return 0;
-      if (Roman.Equals("V")) return 5;
-      if (Roman.Equals("IV")) return 4;
-      if (Roman.Equals("I")) return 1;
-      return 1 + Roman.Substring(1).ToArabic();
+      var Match = Conversions.FirstOrDefault(x => x.Key.Equals(Roman));
+      if (!string.IsNullOrEmpty(Match.Key)) return Match.Value;
+
+      return  1 + Roman.Substring(1).ToArabic();
     }
   }
 }
