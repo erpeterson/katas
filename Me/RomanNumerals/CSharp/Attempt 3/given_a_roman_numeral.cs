0d7203f80b389ffdcf66630900e4999308906ffa
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 16:53:53 -0800

    Attempt 3 (C#): converting IV to 4 fails

--- Me/RomanNumerals/CSharp/Attempt 3/given_a_roman_numeral.cs
+++ Me/RomanNumerals/CSharp/Attempt 3/given_a_roman_numeral.cs
@@ -6,6 +6,7 @@ namespace CSharp.AttemptThree {
   public class given_a_roman_numeral {
     [TestCase("", 0, TestName = "then '' is converted to 0")]
     [TestCase("I", 1, TestName = "then I is converted to 1")]
+    [TestCase("IV", 4, TestName = "then IV is converted to 4")]
     public void when_converting_it_to_an_arabic_numeral(string Roman, int ExpectedArabic) {
       Roman.ToArabic().Should().Be(ExpectedArabic);
     }
