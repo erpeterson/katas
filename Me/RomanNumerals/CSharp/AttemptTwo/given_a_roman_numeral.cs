da66951a3e96a892363802475b60790b0b0a365b
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 16:28:15 -0800

    Attempt 2 (C#): converting V to 5 passes

--- Me/RomanNumerals/CSharp/AttemptTwo/given_a_roman_numeral.cs
+++ Me/RomanNumerals/CSharp/AttemptTwo/given_a_roman_numeral.cs
@@ -20,8 +20,10 @@ namespace CSharp.AttemptTwo {
   public static class RomanNumeralConverter {
     public static int ToArabic(this string Roman) {      
       if(Roman.Equals("IX")) return 9;
+      if(Roman.Equals("V")) return 5;
       if(Roman.Equals("IV")) return 4;
       if(Roman.Equals("I")) return 1;
+
       return  1 + Roman.Substring(1).ToArabic();
     }
   }
