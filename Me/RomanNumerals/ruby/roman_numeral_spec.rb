b8e506cd46d2ebccfa0624fd0ce86292fdf6e22c
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 22:12:42 -0800

    Attempt 1 (Ruby): verify that the rest of the conversions work

--- Me/RomanNumerals/ruby/roman_numeral_spec.rb
+++ Me/RomanNumerals/ruby/roman_numeral_spec.rb
@@ -7,7 +7,23 @@ describe "given a roman numeral" do
       "III" => 3,
       "IV" => 4,
       "V" => 5,
-      "VI" => 6
+      "VI" => 6,
+      "VII" => 7,
+      "VIII" => 8,
+      "IX" => 9,
+      "X" => 10,
+      "XI" => 11,
+      "XIV" => 14,
+      "XV" => 15,
+      "XL" => 40,
+      "L" => 50,
+      "XC" => 90,
+      "C" => 100,
+      "CD" => 400,
+      "D" => 500,
+      "CM" => 900,
+      "M" => 1000,
+      "MCMXCIV" => 1994
     }.each_pair do |roman, arabic|
       it "converts #{roman} string to #{arabic}" do
         expect(convert(roman)).to eq(arabic)
@@ -16,7 +32,10 @@ describe "given a roman numeral" do
   end
 end
 
-Conversions = [["V", 5], ["IV", 4], ["I", 1]]
+Conversions = [["M", 1000], ["CM", 900], ["D", 500],
+               ["CD", 400], ["C", 100], ["XC", 90],
+               ["L", 50], ["XL", 40], ["X", 10],
+               ["IX", 9], ["V", 5], ["IV", 4], ["I", 1]]
 
 def convert(roman)
   return 0 if roman.empty?
