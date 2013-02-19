62f38f283bcfdc7f57a8a11b0778f4b1b50cc296
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 16:50:13 -0800

    Attempt 3 (C#): converting '' to 0 fails

---
+++ Me/RomanNumerals/CSharp/Attempt 3/given_a_roman_numeral.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharp.AttemptThree {
+  [TestFixture]
+  public class given_a_roman_numeral {
+    [TestCase("", 0, TestName = "then '' is converted to 0")]
+    public void when_converting_it_to_an_arabic_numeral(string Roman, int ExpectedArabic) {
+      Roman.ToArabic().Should().Be(ExpectedArabic);
+    }
+  }
+
+  public static class RomanNumeralConverter {   
+    public static int ToArabic(this string Roman) {
+      return -1;
+    }
+  }
+}
