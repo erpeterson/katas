f5abbd8cb4cdb4c210c57ae3ccbcf24d4b9f11be
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 19:07:53 -0800

    Attempt 1 (Ruby): converting V to 5 fails

--- Me/RomanNumerals/ruby/roman_numeral_spec.rb
+++ Me/RomanNumerals/ruby/roman_numeral_spec.rb
@@ -5,7 +5,8 @@ describe "given a roman numeral" do
     {
       "" => 0,
       "I" => 1,
-      "IV" => 4
+      "IV" => 4,
+      "V" => 5
     }.each_pair do |roman, arabic|
       it "converts #{roman} string to #{arabic}" do
         expect(convert(roman)).to eq(arabic)
