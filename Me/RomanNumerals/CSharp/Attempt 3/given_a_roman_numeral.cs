8354d7efea8034d44ce451a7e9973338a0f1189d
Aeden Jameson <aeden.jameson@gmail.com>
2013-02-18 16:51:32 -0800

    Attempt 3 (C#): converting I to 1 fails

--- Me/RomanNumerals/CSharp/Attempt 3/given_a_roman_numeral.cs
+++ Me/RomanNumerals/CSharp/Attempt 3/given_a_roman_numeral.cs
@@ -5,6 +5,7 @@ namespace CSharp.AttemptThree {
   [TestFixture]
   public class given_a_roman_numeral {
     [TestCase("", 0, TestName = "then '' is converted to 0")]
+    [TestCase("I", 1, TestName = "then I is converted to 1")]
     public void when_converting_it_to_an_arabic_numeral(string Roman, int ExpectedArabic) {
       Roman.ToArabic().Should().Be(ExpectedArabic);
     }
