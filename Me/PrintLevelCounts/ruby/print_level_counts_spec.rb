ebb653658c0c8314051169bbf6332b2caf0453b9
Aeden Jameson <aeden.jameson@gmail.com>
2013-03-05 11:36:52 -0800

    Print Level Counts - Attempt #1 (Ruby): level 2 contains 1 node fails

--- Me/PrintLevelCounts/ruby/print_level_counts_spec.rb
+++ Me/PrintLevelCounts/ruby/print_level_counts_spec.rb
@@ -1,12 +1,32 @@
-describe "given only a root node of a binary tree" do
-  before :all do
-    @root = Node.new
+describe "given the root node of a tree" do
+
+  context "when counting the nodes of a tree with one node" do
+    before :all do
+      @root = Node.new
+
+      @counts = count_nodes(@root)
+    end
+
+    it "counts 1 at level 1" do
+      expect(@counts[0]).to eq(1)
+    end
   end
 
-  context "when counting the nodes" do
-      it "counts 1 at level 1" do
-        expect(count_nodes(@root)).to eq(1)
-      end
+  context "when counting the nodes of a tree with two nodes" do
+    before :all do
+      @root = Node.new
+      @root.left = Node.new
+
+      @counts = count_nodes(@root)
+    end
+
+    it "counts 1 at level 1" do
+      expect(@counts[0]).to eq(1)
+    end
+
+    it "counts 1 at level 2" do
+      expect(@counts[1]).to eq(1)
+    end
   end
 end
 
