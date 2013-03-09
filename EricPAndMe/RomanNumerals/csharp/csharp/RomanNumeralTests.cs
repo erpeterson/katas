731f29e3336d34b1cf837132a171c97243bf931a
Eric Peterson <erpeterson@gmail.com>
2013-03-09 13:20:44 -0800

    Roman Numerals Kata - Attempt 1 (C#): coverting ii to 2 passes

--- EricPAndMe/RomanNumerals/csharp/csharp/RomanNumeralTests.cs
+++ EricPAndMe/RomanNumerals/csharp/csharp/RomanNumeralTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace csharp
@@ -25,15 +27,57 @@ namespace csharp
     {
         public int ToArabic(string numeral)
         {
-            switch (numeral)
+            var rules = new IRule[] {new IRules(), new VRules() };
+            var numbers = new Queue<int>();
+
+            foreach (var letter in numeral.ToCharArray())
             {
-                case "i":
-                    return 1;
-                case "v":
-                    return 5;
-                default:
+                var rule = rules.FirstOrDefault(x => x.CanHandle(letter));
+                if (rule != null)
+                {
+                    numbers.Enqueue(rule.GetNumber());
+                }
+                else
+                {
                     throw new NotImplementedException();
+                }
+            }
+
+            var total = numbers.Sum(x => x);
+
+            return total;
+        }
+
+        private class IRules : IRule
+        {
+            public bool CanHandle(char letter)
+            {
+                return letter == 'i';
+            }
+
+            public int GetNumber()
+            {
+                return 1;
             }
         }
+    }
+
+    public class VRules : IRule
+    {
+        public bool CanHandle(char letter)
+        {
+            return letter == 'v';
+        }
+
+        public int GetNumber()
+        {
+            return 5;
+        }
+    }
+
+    internal interface IRule
+    {
+        bool CanHandle(char letter);
+        int GetNumber();
     }
 }
